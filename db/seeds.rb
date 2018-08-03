14557aaeb048aaba840da1adc8eedb4dde534814
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-03 16:23:26 -0700

    debug seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 taxonomies = BirdScraper.scrape_taxonomies("https://www.audubon.org/bird-guide")
 regions = BirdScraper.scrape_regions("https://www.audubon.org/bird-guide")
 bird_names_by_region = BirdScraper.scrape_bird_names_by_region(regions)
@@ -20,8 +12,11 @@ regions.each{|region_tid, region_name| Region.create(name: region_name)}
 birds.each{|bird| Bird.create(bird)}
 
 Region.all.each do |region|
-  bird_names_by_region[region].map do |reg, bird_names|
-    region.birds << bird_names.map{|bird_name| Bird.find_by(:common_name => bird_name)}.compact
-    region.save
+  bird_names_by_region[region.name].each do |bird_name|
+    bird = Bird.find_by(:common_name => bird_name) 
+    if bird
+      region.birds << bird
+      region.save
+    end
   end
 end

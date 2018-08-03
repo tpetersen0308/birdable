e68fa5ee423af585f266624c090f6ddb13118ba3
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-02 17:34:00 -0700

    add scrape_taxonomies method to bird scraper to associate bird families with tid url parameter to scrape for birds by family

--- app/models/bird_scraper.rb
+++ app/models/bird_scraper.rb
@@ -1,7 +1,17 @@
+require 'nokogiri'
+require 'open-uri'
+
 class BirdScraper < ApplicationRecord
 
   def self.get_page(url)
     Nokogiri::HTML(open(url))
   end
 
+  def self.scrape_taxonomies(url)
+    taxonomies = {}
+    self.get_page(url).css("select#edit-field-bird-family-tid option").each.with_index do |taxon, i|
+      next if i == 0  
+      taxonomies[taxon.attributes.values[0].value] = taxon.text
+    end
+  end
 end

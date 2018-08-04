0ac65bb9778cab49a970dd7f0964e0a193153e3c
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-03 17:19:28 -0700

    add audubon_url method to bird class to create link for more info about a given bird

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,4 +1,8 @@
 class Bird < ApplicationRecord
   has_many :birds_regions
   has_many :regions, through: :birds_regions
+
+  def audubon_url
+    "https://www.audubon.org/field-guide/bird/" + self.common_name.gsub(' ', '-').gsub(/[^a-z\-]/, '')
+  end
 end

5599b3014ff0805e9ddf57b4b620d1c1fbb76b0b
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-03 18:13:33 -0700

    build index and show routes in birds controller for API endpoints

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,10 +1,14 @@
 class BirdsController < ApplicationController
   
   def index
+    @birds = Bird.all
 
+    render json: @birds
   end
 
   def show
+    @bird = Bird.all.select{|bird| bird.url_safe_common_name == params[:common_name]}
 
+    render json: @bird
   end
 end

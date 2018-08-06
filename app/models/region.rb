27afa22995e266f9fce1767dcc037d959bbf38b1
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-06 15:31:24 -0700

    add regions global constant

--- app/models/region.rb
+++ app/models/region.rb
@@ -3,4 +3,5 @@ class Region < ApplicationRecord
   has_many :birds, through: :birds_regions
   include Formattable
 
+  REGIONS = ["alaska and the north", "california", "eastern canada", "florida", "great lakes", "mid atlantic", "new england", "northwest", "plains", "rocky mountains", "southeast", "southwest", "texas", "western canada"]
 end

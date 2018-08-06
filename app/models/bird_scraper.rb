ca740831f8edb428870243170da2fb97844a974d
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-06 16:22:49 -0700

    add comments to scraper class

--- app/models/bird_scraper.rb
+++ app/models/bird_scraper.rb
@@ -1,12 +1,19 @@
 require 'nokogiri'
 require 'open-uri'
 
+# class BirdScraper provides class methods that use Nokogiri to scrape information 
+# from the Audubon Society's North American Bird Guide.
 class BirdScraper < ApplicationRecord
 
+  # BirdScraper.get_page returns the html response from the url argument.
   def self.get_page(url)
     Nokogiri::HTML(open(url))
   end
 
+  # BirdScraper.scrape_taxonomies scrapes the taxonomy dropdown list of the guide
+  # and returns a hash of the family_tid attributes and the associated family names
+  # which will be used to build the urls that are needed to scrape bird information
+  # by taxonomic family.
   def self.scrape_taxonomies(url)
     taxonomies = {}
     self.get_page(url).css("select#edit-field-bird-family-tid option").each.with_index do |taxon, i|
@@ -16,6 +23,10 @@ class BirdScraper < ApplicationRecord
     return taxonomies
   end
 
+  # BirdScraper.scrape_regions scrapes the region dropdown list of the guide
+  # and returns a hash of the region_tid attributes and the associated region names
+  # which will be used to build the urls that are needed to scrape bird information
+  # by taxonomic region.
   def self.scrape_regions(url)
     regions = {}
     self.get_page(url).css("select#edit-field-bird-region-tid option").each.with_index do |region, i|
@@ -25,6 +36,12 @@ class BirdScraper < ApplicationRecord
     return regions
   end
   
+  # BirdScraper.scrape_bird_names_by_region takes a region hash as an argument 
+  # and iterates through it, creating a new url for each regional sub-page of the guide
+  # for a list of the names of birds in each region. It accounts for infinite scrolling
+  # with a while loop that increments the page number until all birds from a given region
+  # have been collected, and returns a hash of region names pointing to arrays of
+  # associated bird names.
   def self.scrape_bird_names_by_region(regions)
     birds_by_region = {}
     
@@ -49,6 +66,9 @@ class BirdScraper < ApplicationRecord
     return birds_by_region
   end
 
+  # BirdScraper.scrape_birds_by_family_tid scrapes bird data from a taxonomy page determined
+  # by its family_tid argument, and returns an array of hashes containing data for each bird
+  # on the page for which a song sample is included.
   def self.scrape_birds_by_family_tid(family_tid, family_name)
     url = "https://www.audubon.org/bird-guide?field_bird_family_tid=" + family_tid
     doc = self.get_page(url)
--- app/models/concerns/formattable.rb
+++ app/models/concerns/formattable.rb
@@ -1,3 +1,4 @@
+# module Formattable provides a method to reformat a string to be url-friendly
 module Formattable
   def url_safe_attribute(attr)
     self.send(attr).gsub(' ', '-').gsub(/[^a-z\-]/, '')

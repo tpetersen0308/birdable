8ba16d0ae6421d8f62a26a5c1620519aef6b5ec1
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-06 14:22:19 -0700

    add url_safe_name instance method to region class

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,4 +1,8 @@
 class Region < ApplicationRecord
   has_many :birds_regions
   has_many :birds, through: :birds_regions
+
+  def url_safe_name
+    self.name.gsub(' ', '-').gsub(/[^a-z\-]/, '')
+  end
 end

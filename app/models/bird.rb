1cdab67746f6dd39943de3607229707ee0cfa7dd
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-06 15:42:28 -0700

    refactor taxonomic families constant to contain url safe strings

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -4,14 +4,17 @@ class Bird < ApplicationRecord
   after_initialize :add_url
   include Formattable
 
-  TAXONOMIC_FAMILIES = ["albatrosses", "anhingas", "auks, murres, puffins", "barn owls", "becards, tityras and allies", "blackbirds and orioles", "boobies and gannets", "bushtits", "cardinals, grosbeaks and buntings", "chachalacas", "chickadees and titmice", "cormorants", "cranes", "creepers", "crows, magpies, jays", "cuckoos, roadrunners, anis", "dippers", "ducks and geese", "falcons", "finches", "frigatebirds", "gnatcatchers", "grebes", "gulls and terns", "hawks and eagles", "herons, egrets, bitterns", "hummingbirds", "ibises and spoonbills", "kingfishers", "kinglets", "larks", "leaf-warblers", "longspurs and snow buntings", "loons", "lovebirds and australasian parrots", "mockingbirds and thrashers", "new world quail", "new world sparrows", "new world vultures", "nightjars", "northern storm-petrels", "nuthatches", "old world flycatchers", "old world sparrows", "olive warblers", "ospreys", "owls", "oystercatchers", "pelicans", "pheasants and grouse", "pigeons and doves", "plovers", "rails, gallinules, coots", "sandpipers", "shearwaters and petrels", "shrikes", "silky-flycatchers", "skuas and jaegers", "southern storm-petrels", "starlings and mynas", "stilts and avocets", "storks", "swallows", "swifts", "sylviid warblers", "thrushes", "trogons", "tropicbirds", "tyrant flycatchers", "verdins", "vireos", "wagtails and pipits", "waxwings", "wood warblers", "woodpeckers", "wrens", "yellow-breasted chats"]
-  
+  TAXONOMIC_FAMILIES = ["albatrosses", "anhingas", "auks-murres-puffins", "barn-owls", "becards-tityras-and-allies", "blackbirds-and-orioles", "boobies-and-gannets", "bushtits", "cardinals-grosbeaks-and-buntings", "chachalacas", "chickadees-and-titmice", "cormorants", "cranes", "creepers", "crows-magpies-jays", "cuckoos-roadrunners-anis", "dippers", "ducks-and-geese", "falcons", "finches", "frigatebirds", "gnatcatchers", "grebes", "gulls-and-terns", "hawks-and-eagles", "herons-egrets-bitterns", "hummingbirds", "ibises-and-spoonbills", "kingfishers", "kinglets", "larks", "leaf-warblers", "longspurs-and-snow-buntings", "loons", "lovebirds-and-australasian-parrots", "mockingbirds-and-thrashers", "new-world-quail", "new-world-sparrows", "new-world-vultures", "nightjars", "northern-storm-petrels", "nuthatches", "old-world-flycatchers", "old-world-sparrows", "olive-warblers", "ospreys", "owls", "oystercatchers", "pelicans", "pheasants-and-grouse", "pigeons-and-doves", "plovers", "rails-gallinules-coots", "sandpipers", "shearwaters-and-petrels", "shrikes", "silky-flycatchers", "skuas-and-jaegers", "southern-storm-petrels", "starlings-and-mynas", "stilts-and-avocets", "storks", "swallows", "swifts", "sylviid-warblers", "thrushes", "trogons", "tropicbirds", "tyrant-flycatchers", "verdins", "vireos", "wagtails-and-pipits", "waxwings", "wood-warblers", "woodpeckers", "wrens", "yellow-breasted-chats"]
   def add_url
     self.update(url: self.audubon_url)
   end 
 
   def audubon_url
     "https://www.audubon.org/field-guide/bird/" + self.url_safe_attribute("common_name")
+  end
+
+  def self.taxonomic_families
+    TAXONOMIC_FAMILIES
   end
 
   def self.by_region(region_name)

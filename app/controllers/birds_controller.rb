af7d1c0922ecea1bc727d5fcdd633ae2c0011b12
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-06 14:22:45 -0700

    add nested index route for birds by region to birds controller

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,7 +1,13 @@
 class BirdsController < ApplicationController
   
   def index
-    @birds = Bird.all
+    if params[:region]
+      @birds = Bird.all.select do |bird| 
+        bird.regions.map{|region| region.url_safe_name}.include?(params[:region])
+      end.sort_by{|bird| bird.common_name}
+    else
+      @birds = Bird.all.sort_by{|bird| bird.common_name}
+    end
 
     render json: @birds
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/birds" => "birds#index"
+  get "/birds/:region" => "birds#index"
   get "/birds/:common_name" => "birds#show"
   
 end

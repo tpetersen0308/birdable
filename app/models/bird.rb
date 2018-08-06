24b298618405f9ecd7484e9a76a04586222d5b1f
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-06 16:27:29 -0700

    add comments to bird class

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -4,11 +4,16 @@ class Bird < ApplicationRecord
   after_initialize :add_url
   include Formattable
 
+  # TAXONOMIC FAMILIES constant contains names of all taxonomic family names from the
+  # Audubon Society's North American Bird Guide formatted as url-safe strings.
   TAXONOMIC_FAMILIES = ["albatrosses", "anhingas", "auks-murres-puffins", "barn-owls", "becards-tityras-and-allies", "blackbirds-and-orioles", "boobies-and-gannets", "bushtits", "cardinals-grosbeaks-and-buntings", "chachalacas", "chickadees-and-titmice", "cormorants", "cranes", "creepers", "crows-magpies-jays", "cuckoos-roadrunners-anis", "dippers", "ducks-and-geese", "falcons", "finches", "frigatebirds", "gnatcatchers", "grebes", "gulls-and-terns", "hawks-and-eagles", "herons-egrets-bitterns", "hummingbirds", "ibises-and-spoonbills", "kingfishers", "kinglets", "larks", "leaf-warblers", "longspurs-and-snow-buntings", "loons", "lovebirds-and-australasian-parrots", "mockingbirds-and-thrashers", "new-world-quail", "new-world-sparrows", "new-world-vultures", "nightjars", "northern-storm-petrels", "nuthatches", "old-world-flycatchers", "old-world-sparrows", "olive-warblers", "ospreys", "owls", "oystercatchers", "pelicans", "pheasants-and-grouse", "pigeons-and-doves", "plovers", "rails-gallinules-coots", "sandpipers", "shearwaters-and-petrels", "shrikes", "silky-flycatchers", "skuas-and-jaegers", "southern-storm-petrels", "starlings-and-mynas", "stilts-and-avocets", "storks", "swallows", "swifts", "sylviid-warblers", "thrushes", "trogons", "tropicbirds", "tyrant-flycatchers", "verdins", "vireos", "wagtails-and-pipits", "waxwings", "wood-warblers", "woodpeckers", "wrens", "yellow-breasted-chats"]
+  
+  # add_url sets the url attribute of a bird to its corresponding page in the guide
   def add_url
     self.update(url: self.audubon_url)
   end 
 
+  # audubon_url returns the url of a bird's corresponding page in the guide
   def audubon_url
     "https://www.audubon.org/field-guide/bird/" + self.url_safe_attribute("common_name")
   end
@@ -17,12 +22,14 @@ class Bird < ApplicationRecord
     TAXONOMIC_FAMILIES
   end
 
+  # Bird.by_region returns an array of all birds associated with a given region
   def self.by_region(region_name)
     self.all.select do |bird| 
       bird.regions.map{|region| region.url_safe_attribute("name")}.include?(region_name)
     end.sort_by{|bird| bird.common_name}
   end
 
+  # Bird.by_family returns an array of all birds classified under the same taxonomy
   def self.by_family(family_name)
     self.all.select{|bird| bird.url_safe_attribute("family") == family_name}.sort_by{|bird| bird.common_name}
   end

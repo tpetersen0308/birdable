39698de9617d96fd02fe1bcaee47f325365a1aeb
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-07 16:37:08 -0700

    create store with birdsReducer and pass to Provider to make available to components

--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,16 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import registerServiceWorker from './registerServiceWorker';
+import birdsReducer from './reducers/birdsReducer';
+
+const store = createStore(
+  birdsReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk),
+);
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')

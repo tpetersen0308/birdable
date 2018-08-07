e6ef463ed265bcd4b3a4da477cc9cb4cdfd11170
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-07 16:23:56 -0700

    import redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));

050dfd2a3079fdc2a314f1771e166431e163d32e
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-07 16:34:41 -0700

    refactor birdsReducer to handle LOADING_BIRDS action type

--- client/src/reducers/birdsReducer.js
+++ client/src/reducers/birdsReducer.js
@@ -1,7 +1,9 @@
-function birdsReducer(state = { birds: [] }, action) {
+function birdsReducer(state = { loading: false, birds: [] }, action) {
   switch (action.type) {
+    case 'LOADING_BIRDS':
+      return Object.assign({}, state, { loading: true })
     case "FETCH_BIRDS":
-      return { birds: action.payload }
+      return { loading: false, birds: action.payload }
     default:
       return state
   }

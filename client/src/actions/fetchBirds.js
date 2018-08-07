fa0a39509d0c12286d5dd210d77d02d73725914c
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-07 16:32:26 -0700

    add fetchBirds action to request birds from api

---
+++ client/src/actions/fetchBirds.js
@@ -0,0 +1,12 @@
+import fetch from 'isomorphic-fetch';
+
+export function fetchBirds() {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_BIRDS' });
+    return fetch('http://localhost:3000/api/birds')
+      .then(response => response.json())
+      .then(birds => {
+        dispatch({ type: 'FETCH_BIRDS', payload: birds })
+      });
+  };
+}

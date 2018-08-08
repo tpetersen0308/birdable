82345cda88f8ce0f6843a3fa78e9c645284f1179
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-08 16:01:05 -0700

    fix import paths that were broken by moving components into new folders

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../App.css';
 import { connect } from 'react-redux';
-import { fetchBirds } from './actions/fetchBirds.js';
+import { fetchBirds } from '../actions/fetchBirds.js';
 import NavBar from './NavBar.js';
 
 class App extends Component {

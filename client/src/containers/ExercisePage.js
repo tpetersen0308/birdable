6d2cf3b395c017abcc9a4e10fdd6be29ae182d28
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-09 16:40:55 -0700

    refactor menu options as multiple select

--- client/src/containers/ExercisePage.js
+++ client/src/containers/ExercisePage.js
@@ -1,27 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
+import { Form, FormGroup, ControlLabel, FormControl, Checkbox } from 'react-bootstrap';
 import '../index.css';
 
 class ExercisePage extends Component {
   render() {
-    const taxonomicFamilies = ["albatrosses", "anhingas", "auks-murres-puffins", "barn-owls", "becards-tityras-and-allies", "blackbirds-and-orioles", "boobies-and-gannets", "bushtits", "cardinals-grosbeaks-and-buntings", "chachalacas", "chickadees-and-titmice", "cormorants", "cranes", "creepers", "crows-magpies-jays", "cuckoos-roadrunners-anis", "dippers", "ducks-and-geese", "falcons", "finches", "frigatebirds", "gnatcatchers", "grebes", "gulls-and-terns", "hawks-and-eagles", "herons-egrets-bitterns", "hummingbirds", "ibises-and-spoonbills", "kingfishers", "kinglets", "larks", "leaf-warblers", "longspurs-and-snow-buntings", "loons", "lovebirds-and-australasian-parrots", "mockingbirds-and-thrashers", "new-world-quail", "new-world-sparrows", "new-world-vultures", "nightjars", "northern-storm-petrels", "nuthatches", "old-world-flycatchers", "old-world-sparrows", "olive-warblers", "ospreys", "owls", "oystercatchers", "pelicans", "pheasants-and-grouse", "pigeons-and-doves", "plovers", "rails-gallinules-coots", "sandpipers", "shearwaters-and-petrels", "shrikes", "silky-flycatchers", "skuas-and-jaegers", "southern-storm-petrels", "starlings-and-mynas", "stilts-and-avocets", "storks", "swallows", "swifts", "sylviid-warblers", "thrushes", "trogons", "tropicbirds", "tyrant-flycatchers", "verdins", "vireos", "wagtails-and-pipits", "waxwings", "wood-warblers", "woodpeckers", "wrens", "yellow-breasted-chats"];
-    const regions = ["alaska-and-the-north", "california", "eastern-canada", "florida", "great-lakes", "mid-atlantic", "new-england", "northwest", "plains", "rocky-mountains", "southeast", "southwest", "texas", "western-canada"];
-    const familyMenuItems = taxonomicFamilies.map(family => <option value={family}>{family}</option>)
-    const regionMenuItems = regions.map(region => <MenuItem>{region}</MenuItem>)
+    const taxonomicFamilies = ["all", "albatrosses", "anhingas", "auks-murres-puffins", "barn-owls", "becards-tityras-and-allies", "blackbirds-and-orioles", "boobies-and-gannets", "bushtits", "cardinals-grosbeaks-and-buntings", "chachalacas", "chickadees-and-titmice", "cormorants", "cranes", "creepers", "crows-magpies-jays", "cuckoos-roadrunners-anis", "dippers", "ducks-and-geese", "falcons", "finches", "frigatebirds", "gnatcatchers", "grebes", "gulls-and-terns", "hawks-and-eagles", "herons-egrets-bitterns", "hummingbirds", "ibises-and-spoonbills", "kingfishers", "kinglets", "larks", "leaf-warblers", "longspurs-and-snow-buntings", "loons", "lovebirds-and-australasian-parrots", "mockingbirds-and-thrashers", "new-world-quail", "new-world-sparrows", "new-world-vultures", "nightjars", "northern-storm-petrels", "nuthatches", "old-world-flycatchers", "old-world-sparrows", "olive-warblers", "ospreys", "owls", "oystercatchers", "pelicans", "pheasants-and-grouse", "pigeons-and-doves", "plovers", "rails-gallinules-coots", "sandpipers", "shearwaters-and-petrels", "shrikes", "silky-flycatchers", "skuas-and-jaegers", "southern-storm-petrels", "starlings-and-mynas", "stilts-and-avocets", "storks", "swallows", "swifts", "sylviid-warblers", "thrushes", "trogons", "tropicbirds", "tyrant-flycatchers", "verdins", "vireos", "wagtails-and-pipits", "waxwings", "wood-warblers", "woodpeckers", "wrens", "yellow-breasted-chats"];
+    const regions = ["all", "alaska-and-the-north", "california", "eastern-canada", "florida", "great-lakes", "mid-atlantic", "new-england", "northwest", "plains", "rocky-mountains", "southeast", "southwest", "texas", "western-canada"];
+    const familyMenuItems = taxonomicFamilies.map(family => <option><Checkbox value={family}>{family}</Checkbox></option>)
+    const regionMenuItems = regions.map(region => <option><Checkbox value={region}>{region}</Checkbox></option>)
 
     return (
       <div>
         <h4>Select which regions and families you would like to test your knowledge of below:</h4>
         <div class="filter-options">
-          <form>
+          <Form inline>
             <FormGroup controlId="formControlsSelectMultiple">
-              <ControlLabel>Family</ControlLabel>
+              <p><ControlLabel>Families</ControlLabel></p>
               <FormControl componentClass="select" multiple>
-                {/* options... */}
+                {familyMenuItems}
               </FormControl>
             </FormGroup>
-          </form>
+            {"   "}
+            <FormGroup controlId="formControlsSelectMultiple">
+              <p><ControlLabel>Regions</ControlLabel></p>
+              <FormControl componentClass="select" multiple>
+                {regionMenuItems}
+              </FormControl>
+            </FormGroup>
+          </Form>
         </div>
       </div>
     )

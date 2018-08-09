216c278c425c09a71f42b985bcafd2486b43a598
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-08 18:13:30 -0700

    add dropdown buttons to ExercisePage for filtering options

--- client/src/containers/ExercisePage.js
+++ client/src/containers/ExercisePage.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { ButtonToolbar, DropdownButton } from 'react-bootstrap';
 
 class ExercisePage extends Component {
   render() {
     return (
       <div>
-        <h3>Select which regions and families you would like to test your knowledge of below:</h3>
-        {/* ADD DROPDOWN MENUS HERE */}
+        <h4>Select which regions and families you would like to test your knowledge of below:</h4>
+        <ButtonToolbar className="filter-options">
+          <DropdownButton>
+
+          </DropdownButton>
+        </ButtonToolbar>
       </div>
     )
   }

94f392191a9d0a9becc86dca05d436c156032919
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-08 17:28:29 -0700

    refactor bird to conditionally display info based on exercise type

--- client/src/components/Bird.js
+++ client/src/components/Bird.js
@@ -2,18 +2,34 @@ import React from 'react';
 import { Thumbnail } from 'react-bootstrap';
 
 export const Bird = (props) => {
+  let nameInfo = <div>
+    <h3>{props.bird.common_name}</h3>
+    <p>{props.bird.scientific_name}</p>
+  </div>
+
+  let songInfo = <div><audio
+    id={props.bird.id}
+    controls
+    src={props.bird.song}>
+    Your browser does not support the <code>audio</code> element.
+    </audio>
+  </div>
+
+  let birdInfo = exerciseType => {
+    switch (exerciseType) {
+      case "NAME":
+        return <div>{songInfo}</div>
+      case "SONG":
+        return <div>{nameInfo}</div>
+      default:
+        return <div>{nameInfo}{songInfo}</div>
+    }
+  }
+
   return (
     <div>
       <Thumbnail src={props.bird.image} alt={props.bird.common_name}>
-        <h3>{props.bird.common_name}</h3>
-        <p>{props.bird.scientific_name}</p>
-        <p>
-          <audio
-            id={props.bird.id}
-            controls
-            src={props.bird.song}>
-            Your browser does not support the <code>audio</code> element.
-        </audio></p>
+        {birdInfo(props.exerciseType)}
       </Thumbnail>
     </div>
   )

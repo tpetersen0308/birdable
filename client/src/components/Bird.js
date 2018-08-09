cdac2cabada734b6e08f34bb2b85ecfcda90bbf1
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-08 17:14:35 -0700

    build bird component

--- client/src/components/Bird.js
+++ client/src/components/Bird.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Thumbnail } from 'react-bootstrap';
 
 export const Bird = (props) => {
   return (
-    <div></div>
+    <div>
+      <Thumbnail src={props.bird.image} alt={props.bird.common_name}>
+        <h3>{props.bird.common_name}</h3>
+        <p>{props.bird.scientific_name}</p>
+        <p>
+          <audio
+            id={props.bird.id}
+            controls
+            src={props.bird.song}>
+            Your browser does not support the <code>audio</code> element.
+        </audio></p>
+      </Thumbnail>
+    </div>
   )
 }

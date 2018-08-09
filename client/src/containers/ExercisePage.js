f399a7a544f3b8b8fb1921f536f7cd75fc91d968
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-08 18:27:48 -0700

    adjust styling to center dropdown buttons

--- client/src/containers/ExercisePage.js
+++ client/src/containers/ExercisePage.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { ButtonToolbar, DropdownButton } from 'react-bootstrap';
+import '../index.css';
 
 class ExercisePage extends Component {
   render() {
     return (
       <div>
         <h4>Select which regions and families you would like to test your knowledge of below:</h4>
-        <ButtonToolbar className="filter-options">
-          <DropdownButton>
+        <div class="filter-options">
+          <ButtonToolbar>
+            <DropdownButton>
 
-          </DropdownButton>
-        </ButtonToolbar>
+            </DropdownButton>
+            <DropdownButton>
+
+            </DropdownButton>
+          </ButtonToolbar>
+        </div>
       </div>
     )
   }

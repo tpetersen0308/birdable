b0d0b160a85fbfec5ad09ece76085a662c0ecba6
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-10 11:26:13 -0700

    add getBirdsForExercise method to randomly select four birds from collection for new problem

--- client/src/containers/Exercise.js
+++ client/src/containers/Exercise.js
@@ -3,7 +3,18 @@ import { connect } from 'react-redux';
 import { addProblem, addSolution, addUserAnswer } from '../actions/exerciseActions.js';
 
 class Exercise extends Component {
+
+  getBirdsForProblem = birdSelection => {
+    let birds = [];
+    for (let i = 0; i < 4; i++) {
+      birds.push(birdSelection[Math.floor(Math.random() * birdSelection.length)]);
+      birdSelection = birdSelection.filter(bird => bird.common_name !== birds[i].common_name);
+    }
+    return birds;
+  }
+
   render() {
+    debugger
     return (
       <div>New Problem</div>
     )

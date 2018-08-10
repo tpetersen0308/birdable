d45d121437246bfb61ad9d2f5136ac12796d847c
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-09 21:45:01 -0700

    create addExercise and addSolution actions to add current exercise and its solution to the store

--- client/src/actions/exerciseActions.js
+++ client/src/actions/exerciseActions.js
@@ -1,3 +1,11 @@
 export function selectBirds(birds) {
-  return { type: "SELECT BIRDS", payload: birds }
+  return { type: "SELECT_BIRDS", payload: birds }
+}
+
+export function addExercise(exercise) {
+  return { type: "ADD_EXERCISE", payload: exercise }
+}
+
+export function addSolution(solution) {
+  return { type: "ADD_SOLUTION", payload: solution }
 }

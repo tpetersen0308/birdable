54cc3fa6bb6a502e60533852bec6a1f6c60b273f
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-09 18:06:18 -0700

    add onSubmit method to form and stub out handleSubmit

--- client/src/containers/ExercisePage.js
+++ client/src/containers/ExercisePage.js
@@ -41,6 +41,10 @@ class ExercisePage extends Component {
     }, () => console.log(this.state.selectedRegions))
   }
 
+  handleSubmit = event => {
+
+  }
+
   render() {
     const taxonomicFamilies = ["albatrosses", "anhingas", "auks-murres-puffins", "barn-owls", "becards-tityras-and-allies", "blackbirds-and-orioles", "boobies-and-gannets", "bushtits", "cardinals-grosbeaks-and-buntings", "chachalacas", "chickadees-and-titmice", "cormorants", "cranes", "creepers", "crows-magpies-jays", "cuckoos-roadrunners-anis", "dippers", "ducks-and-geese", "falcons", "finches", "frigatebirds", "gnatcatchers", "grebes", "gulls-and-terns", "hawks-and-eagles", "herons-egrets-bitterns", "hummingbirds", "ibises-and-spoonbills", "kingfishers", "kinglets", "larks", "leaf-warblers", "longspurs-and-snow-buntings", "loons", "lovebirds-and-australasian-parrots", "mockingbirds-and-thrashers", "new-world-quail", "new-world-sparrows", "new-world-vultures", "nightjars", "northern-storm-petrels", "nuthatches", "old-world-flycatchers", "old-world-sparrows", "olive-warblers", "ospreys", "owls", "oystercatchers", "pelicans", "pheasants-and-grouse", "pigeons-and-doves", "plovers", "rails-gallinules-coots", "sandpipers", "shearwaters-and-petrels", "shrikes", "silky-flycatchers", "skuas-and-jaegers", "southern-storm-petrels", "starlings-and-mynas", "stilts-and-avocets", "storks", "swallows", "swifts", "sylviid-warblers", "thrushes", "trogons", "tropicbirds", "tyrant-flycatchers", "verdins", "vireos", "wagtails-and-pipits", "waxwings", "wood-warblers", "woodpeckers", "wrens", "yellow-breasted-chats"];
     const regions = ["alaska-and-the-north", "california", "eastern-canada", "florida", "great-lakes", "mid-atlantic", "new-england", "northwest", "plains", "rocky-mountains", "southeast", "southwest", "texas", "western-canada"];
@@ -51,7 +55,7 @@ class ExercisePage extends Component {
       <div>
         <h4>Select which regions and families you would like to test your knowledge of below:</h4>
         <div id="filter-options">
-          <Form inline>
+          <Form inline onSubmit={this.handleSubmit}>
             <FormGroup controlId="formControlsSelectMultiple">
               <p><ControlLabel>Families</ControlLabel></p>
               <FormControl componentClass="select" multiple>

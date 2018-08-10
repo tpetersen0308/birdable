eadc175bc65bd0940ebc7ea83256712119c23a09
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-09 21:37:50 -0700

    add birdSelectionReducer exerciseReducer and solutionReducer to store

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,17 @@
 import { combineReducers } from 'redux';
 import birdsReducer from './birdsReducer';
 import loadingReducer from './loadingReducer.js';
+import birdSelectionReducer from './birdSelectionReducer.js';
+import exerciseReducer from './exerciseReducer.js';
+import solutionReducer from './solutionReducer.js';
 
 const rootReducer = combineReducers({
   birds: birdsReducer,
+  exercise: {
+    birdSelection: birdSelectionReducer,
+    problem: exerciseReducer,
+    solution: solutionReducer,
+  },
   loading: loadingReducer,
 });
 

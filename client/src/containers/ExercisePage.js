9df4ec95af09cfd18917c3306c943360d50de3ff
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-09 17:06:24 -0700

    change onSubmit to onClick in option elements and rename event handlers

--- client/src/containers/ExercisePage.js
+++ client/src/containers/ExercisePage.js
@@ -13,22 +13,22 @@ class ExercisePage extends Component {
     }
   }
 
-  addFamilySelection = event => {
+  selectFamily = event => {
     this.setState({
       ...this.state,
-      selectedFamilies: this.state.selectedFamilies.push(event.target.value)
+      selectedFamilies: this.state.selectedFamilies.concat(event.target.value)
     }, () => console.log(this.state.selectedFamilies))
   }
 
-  addRegionSelection = event => {
+  selectRegion = event => {
 
   }
 
   render() {
     const taxonomicFamilies = ["all", "albatrosses", "anhingas", "auks-murres-puffins", "barn-owls", "becards-tityras-and-allies", "blackbirds-and-orioles", "boobies-and-gannets", "bushtits", "cardinals-grosbeaks-and-buntings", "chachalacas", "chickadees-and-titmice", "cormorants", "cranes", "creepers", "crows-magpies-jays", "cuckoos-roadrunners-anis", "dippers", "ducks-and-geese", "falcons", "finches", "frigatebirds", "gnatcatchers", "grebes", "gulls-and-terns", "hawks-and-eagles", "herons-egrets-bitterns", "hummingbirds", "ibises-and-spoonbills", "kingfishers", "kinglets", "larks", "leaf-warblers", "longspurs-and-snow-buntings", "loons", "lovebirds-and-australasian-parrots", "mockingbirds-and-thrashers", "new-world-quail", "new-world-sparrows", "new-world-vultures", "nightjars", "northern-storm-petrels", "nuthatches", "old-world-flycatchers", "old-world-sparrows", "olive-warblers", "ospreys", "owls", "oystercatchers", "pelicans", "pheasants-and-grouse", "pigeons-and-doves", "plovers", "rails-gallinules-coots", "sandpipers", "shearwaters-and-petrels", "shrikes", "silky-flycatchers", "skuas-and-jaegers", "southern-storm-petrels", "starlings-and-mynas", "stilts-and-avocets", "storks", "swallows", "swifts", "sylviid-warblers", "thrushes", "trogons", "tropicbirds", "tyrant-flycatchers", "verdins", "vireos", "wagtails-and-pipits", "waxwings", "wood-warblers", "woodpeckers", "wrens", "yellow-breasted-chats"];
     const regions = ["all", "alaska-and-the-north", "california", "eastern-canada", "florida", "great-lakes", "mid-atlantic", "new-england", "northwest", "plains", "rocky-mountains", "southeast", "southwest", "texas", "western-canada"];
-    const familyMenuItems = taxonomicFamilies.map(family => <option value={family} onSelect={this.addFamilySelection}>{family}</option>)
-    const regionMenuItems = regions.map(region => <option value={region} onSelect={this.addRegionSelection}>{region}</option>)
+    const familyMenuItems = taxonomicFamilies.map(family => <option value={family} onClick={this.selectFamily}>{family}</option>)
+    const regionMenuItems = regions.map(region => <option value={region} onClick={this.selectRegion}>{region}</option>)
 
     return (
       <div>

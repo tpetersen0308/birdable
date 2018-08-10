c4cd4c2137ca70797d09718e55b35bcc6099af3d
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-10 12:22:22 -0700

    render bird components in problem component

--- client/src/components/Problem.js
+++ client/src/components/Problem.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import { Bird } from './Bird.js';
 
 const Problem = (props) => {
+
+  let birds = props.problem.birds.map(bird => <Bird bird={bird} exerciseType="SONG" />)
+
   return (
-    <div>Problem</div>
+    <div>{birds}</div>
   )
 }
 

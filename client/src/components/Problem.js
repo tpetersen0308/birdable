f75c315ccedbc31c9ac8006159713c30f9c6b89a
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-10 12:53:26 -0700

    add song audio to problem

--- client/src/components/Problem.js
+++ client/src/components/Problem.js
@@ -1,12 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Bird } from './Bird.js';
 
 const Problem = (props) => {
-
   let birds = props.problem.birds.map(bird => <Bird bird={bird} exerciseType="SONG" />)
+  let correctBird = props.problem.birds.filter(bird => bird.id === props.problem.correctAnswerKey)[0]
+  let songAudio = ''
+  if (correctBird) {
+    songAudio = <div>
+      <audio
+        id={props.problem.correctAnswerKey}
+        controls
+        src={correctBird.song}>
+        Your browser does not support the <code>audio</code> element.
+    </audio>
+    </div>
+  }
 
   return (
-    <div>{birds}</div>
+    <div>
+      {birds}
+      {songAudio}
+    </div>
   )
 }
 

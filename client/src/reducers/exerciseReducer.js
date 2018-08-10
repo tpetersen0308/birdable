2601397ea7ab6cf89c3cd9536ebef143d90b903a
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-10 10:39:51 -0700

    add userAnswer to exerciseReducer

--- client/src/reducers/exerciseReducer.js
+++ client/src/reducers/exerciseReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import birdSelectionReducer from './birdSelectionReducer.js';
 import problemReducer from './problemReducer.js';
 import solutionReducer from './solutionReducer.js';
+import userAnswerReducer from './userAnswerReducer.js';
 
 const exerciseReducer = combineReducers({
   birdSelection: birdSelectionReducer,
   problem: problemReducer,
   solution: solutionReducer,
+  userAnswer: userAnswerReducer,
 });
 
 export default exerciseReducer;

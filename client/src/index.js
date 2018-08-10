04f232c7ba07057a968042c12faf9797dd213fab
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-09 18:27:44 -0700

    add function for title casing strings

--- client/src/index.js
+++ client/src/index.js
@@ -14,6 +14,15 @@ const store = createStore(
   applyMiddleware(thunk),
 );
 
+export function toTitleCase(str) {
+  return str.replace(
+    /\w\S*/g,
+    function (txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    }
+  );
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <App />

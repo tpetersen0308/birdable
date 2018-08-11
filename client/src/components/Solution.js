3ad8f17eb8dd894a2b16d8a531607b3bf113e66f
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-11 13:21:23 -0700

    render bird components in solution

--- client/src/components/Solution.js
+++ client/src/components/Solution.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import { Bird } from './Bird.js';
 
 const Solution = (props) => {
+  let birds = props.problem.birds.map(bird => <Bird bird={bird} />)
+
   return (
-    <div>Solution</div>
+    <div>
+      <div className="bird-cards">
+        {birds}
+      </div>
+    </div>
   )
 }
 

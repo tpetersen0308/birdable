ee058446dd8e778ef463f3799b82feb39dddbfdc
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-11 15:06:45 -0700

    refactor logic for assigning id prop to bird component

--- client/src/components/Solution.js
+++ client/src/components/Solution.js
@@ -7,15 +7,15 @@ const Solution = (props) => {
   let correctBird = props.problem.birds.find(bird => bird.id === props.problem.correctAnswerKey);
   let birds = props.problem.birds.map(bird => <Bird
     bird={bird}
-    id={correct ? (bird.id === props.userAnswer ? 'correct-answer' : '')
-      : (bird.id === props.userAnswer ? 'incorrect-answer'
-        : (bird.id === props.problem.correctAnswerKey ? 'correct-answer'
+    id={correct ? (bird === correctBird ? 'correct-answer' : '')
+      : (bird === correctBird ? 'correct-answer'
+        : (bird.id === props.userAnswer ? 'incorrect-answer'
           : ''))}
   />)
 
   return (
     <div>
-      <h4>{correct ? "Correct!" : "Sorry, the correct answer was " + toTitleCase(correctBird.common_name)}:</h4>
+      <h4>{correct ? "Correct!" : "Sorry, the correct answer was " + toTitleCase(correctBird.common_name) + ':'}</h4>
       <div className="bird-cards">
         {birds}
       </div>

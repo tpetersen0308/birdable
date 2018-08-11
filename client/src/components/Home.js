f046d5ac5e36dcb94118c7393cfe48d88547da01
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-11 15:49:45 -0700

    render Bird component in Home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Bird from './Bird.js';
 
 export const Home = (props) => {
   return (
-    <div></div>
+    <div>
+      <br />
+      <br />
+      <h4>Here is a random bird! Look at it, it's cool!</h4>
+      <br />
+      <br />
+      <Bird bird={props.bird} />
+    </div>
   )
 }

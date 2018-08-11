f0c7b0fe59b3e385cff5c4e8b896eafa379abede
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-11 15:01:20 -0700

    display informative feedback when answer is incorrect

--- client/src/components/Solution.js
+++ client/src/components/Solution.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { Bird } from './Bird.js';
+import { toTitleCase } from '../index.js';
 
 const Solution = (props) => {
   let correct = props.correct(props.userAnswer, props.problem.correctAnswerKey);
+  let correctBird = props.problem.birds.find(bird => bird.id === props.problem.correctAnswerKey);
   let birds = props.problem.birds.map(bird => <Bird
     bird={bird}
     id={correct ? (bird.id === props.userAnswer ? 'correct-answer' : '')
@@ -13,7 +15,7 @@ const Solution = (props) => {
 
   return (
     <div>
-      <h4>{correct ? "Correct!" : "Incorrect"}</h4>
+      <h4>{correct ? "Correct!" : "Sorry, the correct answer was " + toTitleCase(correctBird.common_name)}:</h4>
       <div className="bird-cards">
         {birds}
       </div>

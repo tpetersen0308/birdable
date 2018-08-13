5a5a4a2cfc1032a78fec43501e9eae97e4f1ed8f
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-13 16:54:33 -0700

    add form to BirdFilter

--- client/src/components/BirdFilter.js
+++ client/src/components/BirdFilter.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Button, Checkbox, DropdownButton } from 'react-bootstrap';
+import { toTitleCase } from '../index.js';
+
 
 export const BirdFilter = (props) => {
   return (
-    <div></div>
+    <div id="filter-dropdown">
+      <DropdownButton
+        bsStyle="warning"
+        bsSize="large"
+        title="Families"
+      >
+        {props.families.map(family => <Checkbox onChange={props.handleFamilyCheckbox} value={family}>{toTitleCase(family)}</Checkbox>)}
+      </DropdownButton>
+      {'  '}
+      <DropdownButton
+        bsSize="large"
+        bsStyle='warning'
+        title="Regions"
+      >
+        {props.regions.map(region => <Checkbox onChange={props.handleRegionCheckbox} value={region}>{toTitleCase(region)}</Checkbox>)}
+      </DropdownButton>
+      {'  '}
+      <Button bsSize="large" type="submit" onClick={props.handleSubmit}>Go!</Button>
+    </div>
   )
 }

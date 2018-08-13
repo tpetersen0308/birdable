1d524a4af508c688a0954ff1049c9c84c7b5c137
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-13 16:54:55 -0700

    refactor ExercisePage to render filter form through BirdFilter component

--- client/src/containers/ExercisePage.js
+++ client/src/containers/ExercisePage.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Button, Form, FormGroup, ControlLabel, FormControl, Checkbox, DropdownButton, MenuItem } from 'react-bootstrap';
-import { toTitleCase, urlSafeString } from '../index.js';
 import { selectBirds } from '../actions/exerciseActions.js';
+import { BirdFilter } from '../components/BirdFilter.js';
 import '../index.css';
 
 class ExercisePage extends Component {
@@ -89,33 +88,18 @@ class ExercisePage extends Component {
   render() {
     const taxonomicFamilies = ["albatrosses", "anhingas", "auks, murres, puffins", "barn owls", "becards, tityras and allies", "blackbirds and orioles", "boobies and gannets", "bushtits", "cardinals, grosbeaks and buntings", "chachalacas", "chickadees and titmice", "cormorants", "cranes", "creepers", "crows, magpies, jays", "cuckoos, roadrunners, anis", "dippers", "ducks and geese", "falcons", "finches", "frigatebirds", "gnatcatchers", "grebes", "gulls and terns", "hawks and eagles", "herons, egrets, bitterns", "hummingbirds", "ibises and spoonbills", "kingfishers", "kinglets", "larks", "leaf-warblers", "longspurs and snow buntings", "loons", "lovebirds and australasian parrots", "mockingbirds and thrashers", "new world quail", "new world sparrows", "new world vultures", "nightjars", "northern storm-petrels", "nuthatches", "old world flycatchers", "old world sparrows", "olive warblers", "ospreys", "owls", "oystercatchers", "pelicans", "pheasants and grouse", "pigeons and doves", "plovers", "rails, gallinules, coots", "sandpipers", "shearwaters and petrels", "shrikes", "silky-flycatchers", "skuas and jaegers", "southern storm-petrels", "starlings and mynas", "stilts and avocets", "storks", "swallows", "swifts", "sylviid warblers", "thrushes", "trogons", "tropicbirds", "tyrant flycatchers", "verdins", "vireos", "wagtails and pipits", "waxwings", "wood warblers", "woodpeckers", "wrens", "yellow-breasted chats"];
     const regions = ["alaska and the north", "california", "eastern canada", "florida", "great lakes", "mid atlantic", "new england", "northwest", "plains", "rocky mountains", "southeast", "southwest", "texas", "western canada"];
-    const familyMenuItems = taxonomicFamilies.map(family => <option value={family} onClick={this.selectFamily}>{toTitleCase(family)}</option>)
-    const regionMenuItems = regions.map(region => <option value={region} onClick={this.selectRegion}>{toTitleCase(region)}</option>)
 
     return (
       <div>
         <br />
         <h4>Select families and regions to practice identifying:</h4>
         <br />
-        <div id="filter-dropdown">
-          <DropdownButton
-            bsStyle="warning"
-            bsSize="large"
-            title="Families"
-          >
-            {taxonomicFamilies.map(family => <Checkbox onChange={this.handleFamilyCheckbox} value={family}>{toTitleCase(family)}</Checkbox>)}
-          </DropdownButton>
-          {'  '}
-          <DropdownButton
-            bsSize="large"
-            bsStyle='warning'
-            title="Regions"
-          >
-            {regions.map(region => <Checkbox onChange={this.handleRegionCheckbox} value={region}>{toTitleCase(region)}</Checkbox>)}
-          </DropdownButton>
-          {'  '}
-          <Button bsSize="large" type="submit" onClick={this.handleSubmit}>Go!</Button>
-        </div>
+        <BirdFilter
+          families={taxonomicFamilies}
+          regions={regions}
+          handleFamilyCheckbox={this.handleFamilyCheckbox}
+          handleRegionCheckbox={this.handleRegionCheckbox}
+          handleSubmit={this.handleSubmit} />
       </div>
 
     )

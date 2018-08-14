2e882b960b8097b653e2f8c410ad2b212f24d812
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-13 18:54:10 -0700

    pass submitForBrowse to BirdsFilter as props to determine action it will fire in handleSubmit

--- client/src/containers/BirdsPage.js
+++ client/src/containers/BirdsPage.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import BirdsFilter from './BirdsFilter';
+import { selectBirdsForBrowse } from '../actions/browseActions.js';
 
 class BirdsPage extends Component {
 
   submitForBrowse = () => {
     this.props.history.push('/birds');
   }
+
   render() {
     return (
       <div>
         <br />
-        <h4>Select families and regions to browse, or click 'Go!' to see them all!</h4>
+        <h4>Select families and regions to browse, or hit 'Go!' to see them all!</h4>
         <br />
-        <BirdsFilter handleSubmitRoute={this.submitForBrowse} />
+        <BirdsFilter handleSubmitRoute={this.submitForBrowse} selectAction={this.props.selectBirdsForBrowse} />
       </div>
     )
   }
 }
 
-export default BirdsPage;
+function mapDispatchToProps(dispatch) {
+  return {
+    selectBirdsForBrowse: (birds) => dispatch(selectBirdsForBrowse(birds)),
+  }
+}
+
+export default connect(null, mapDispatchToProps)(BirdsPage);

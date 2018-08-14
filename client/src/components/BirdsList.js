ffcfe4090a13972d9913847c62016673b691a98a
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-13 19:04:15 -0700

    render birds in BirdsList

--- client/src/components/BirdsList.js
+++ client/src/components/BirdsList.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Bird } from './Bird.js';
 
 export const BirdsList = (props) => {
+  let birds = props.birds.map(bird => <Bird id='browse-bird' bird={bird} />)
   return (
-    <div></div>
+    <div>{birds}</div>
   )
 }

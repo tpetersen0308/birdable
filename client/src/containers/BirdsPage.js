8c1ebfe3b43e4ad4684ece739afb3ca83b20783a
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-13 18:19:05 -0700

    add BirdsFilter and submitForBrowse to BirdsPage

--- client/src/containers/BirdsPage.js
+++ client/src/containers/BirdsPage.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import BirdsFilter from './BirdsFilter';
 
 class BirdsPage extends Component {
+
+  submitForBrowse = () => {
+    this.props.history.push('/birds');
+  }
   render() {
     return (
-      <div>Birds Page</div>
+      <div>
+        <br />
+        <h4>Select families and regions to browse, or click 'Go!' to see them all!</h4>
+        <br />
+        <BirdsFilter handleSubmitRoute={this.submitForBrowse} />
+      </div>
     )
   }
 }
 
-export default BirdsPage; 
+export default BirdsPage;

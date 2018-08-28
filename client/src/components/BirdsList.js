5cc01b7e3547e14d7dd2354b094076a79a0459d0
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-28 15:31:41 -0700

    add key props to children of iterators to debug console warnings

--- client/src/components/BirdsList.js
+++ client/src/components/BirdsList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Bird } from './Bird.js';
 
 export const BirdsList = (props) => {
-  let birds = props.birds.map(bird => <Bird bird={bird} />)
+  let birds = props.birds.map(bird => <Bird key={bird.id} bird={bird} />)
   return (
     <div className='birds-list'>{birds}</div>
   )

d8b5e232453c23eaf95b57c044b890c242e03e17
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-30 16:30:26 -0700

    add comments to functions

--- client/src/actions/birdActions.js
+++ client/src/actions/birdActions.js
@@ -1,6 +1,10 @@
 import fetch from 'isomorphic-fetch';
 import { toTitleCase } from '../index.js';
 
+/* 
+    fetchBirds() fires GET request to the Rails API for the bird collection
+    and dispatches an action to add the collection to state.
+*/
 export function fetchBirds() {
   return (dispatch) => {
     dispatch({ type: 'LOADING_BIRDS' });
@@ -13,6 +17,10 @@ export function fetchBirds() {
   };
 }
 
+/*
+    updateBirdStats() fires a PUT request to the Rails API to update answer 
+    stats for a given bird.
+*/
 export function updateBirdStats(bird_id, data) {
   return fetch(`/api/v1/birds/${bird_id}`, {
     method: "PUT",

fbdabbd0fb42cdcec4a7ffa83e0ceaf02f2396ac
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-30 12:28:39 -0700

    add correct_answers and incorrect_anwsers to bird serializer

--- app/serializers/bird_serializer.rb
+++ app/serializers/bird_serializer.rb
@@ -1,4 +1,4 @@
 class BirdSerializer < ActiveModel::Serializer
-  attributes :id, :common_name, :scientific_name, :family, :image, :song, :url
+  attributes :id, :common_name, :scientific_name, :family, :image, :song, :url, :correct_answers, :incorrect_answers
   has_many :regions
 end

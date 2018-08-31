7e771e338d2a99ea056ebbd0eac06a7815efe6f3
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-30 17:03:12 -0700

    add comments to Solution component

--- client/src/components/Solution.js
+++ client/src/components/Solution.js
@@ -1,11 +1,27 @@
+/*
+    Solution is a stateless presentational component that renders the content
+    for the solution portion of an exercise: all of the birds that were answer
+    options, with all name and song information displayed for the user to review
+    after submitting an answer, including feedback on the exercise's results.
+*/
+
 import React from 'react';
 import { Bird } from './Bird.js';
 import { toTitleCase } from '../index.js';
 import { Button } from 'react-bootstrap';
 
 const Solution = (props) => {
+  // determine if the answer was correct using function passed in as props.
   let correct = props.correct(props.userAnswer, props.problem.correctAnswerKey);
+
+  // the correct bird is the one with an id matching the problem's correctAnswerKey.
   let correctBird = props.problem.birds.find(bird => bird.id === props.problem.correctAnswerKey);
+
+  /* 
+      map Bird components from the collection passed in as props, with id's
+      determined conditionally for the correct and, if applicable, incorrect
+      answer(s), so the results can be properly styled.
+  */
   let birds = props.problem.birds.map(bird => <Bird
     bird={bird}
     key={bird.id}

65313a379b709979ecd91434d9ed9ca9c66d2f31
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-30 18:41:57 -0700

    add comments to userAnswerReducer()

--- client/src/reducers/userAnswerReducer.js
+++ client/src/reducers/userAnswerReducer.js
@@ -1,3 +1,5 @@
+// userAnswerReducer() adds the user's answer to the current problem to state.
+
 function userAnswerReducer(state = null, action) {
   switch (action.type) {
     case "ADD_USER_ANSWER":

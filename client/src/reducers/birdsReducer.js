f7d91e0987e65f8791e1d69494813d9c06287d2a
tpetersen0308 <travis.e.petersen@gmail.com>
2018-08-30 18:36:19 -0700

    add comments to birdsReducer()

--- client/src/reducers/birdsReducer.js
+++ client/src/reducers/birdsReducer.js
@@ -1,3 +1,8 @@
+/* 
+    birdsReducer() returns the action's payload of an array of bird
+    objects from GET request to Rails API to be added to state.
+*/
+
 function birdsReducer(state = [], action) {
   switch (action.type) {
     case "FETCH_BIRDS":

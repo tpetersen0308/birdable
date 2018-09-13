dc4fed2e404f2e9ee70976a4030d660dd5a771a2
tpetersen0308 <travis.e.petersen@gmail.com>
2018-09-13 16:53:48 -0700

    add type property to exerciseReducer in order to determine how to load problem component

--- client/src/reducers/exerciseReducer.js
+++ client/src/reducers/exerciseReducer.js
@@ -7,11 +7,13 @@ import { combineReducers } from 'redux';
 import birdsForExerciseReducer from './birdsForExerciseReducer.js';
 import problemReducer from './problemReducer.js';
 import userAnswerReducer from './userAnswerReducer.js';
+import exerciseTypeReducer from './exerciseTypeReducer.js';
 
 const exerciseReducer = combineReducers({
   birdSelection: birdsForExerciseReducer,
   problem: problemReducer,
   userAnswer: userAnswerReducer,
+  type: exerciseTypeReducer,
 });
 
 export default exerciseReducer;

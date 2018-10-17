6067f4845ff423e145bf8a308e5f959f0d1aca9f
tpetersen0308 <travis.e.petersen@gmail.com>
2018-10-17 15:31:33 -0700

    add create action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: 201
+    else
+      render :json => { :errors => @user.errors}, status: 422
+    end
+  end
 end

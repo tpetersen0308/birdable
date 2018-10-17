21dcbb464253fa4c5b716ad280e94ecc793855d5
tpetersen0308 <travis.e.petersen@gmail.com>
2018-10-17 16:42:19 -0700

    create reducer to add a user to the redux store

---
+++ client/src/reducers/usersReducer.js
@@ -0,0 +1,12 @@
+// usersReducer() adds the current user to the redux store
+
+function usersReducer(state = null, action) {
+  switch (action.type) {
+    case "ADD_USER":
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+export default usersReducer;

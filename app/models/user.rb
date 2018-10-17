51291a4e255ab5328db88c9cc73f99c262344f12
tpetersen0308 <travis.e.petersen@gmail.com>
2018-10-17 15:20:00 -0700

    add case-insensitive find scope to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: {case_sensitive: false}
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
-  
+
+  scope :ci_find, lambda { |attribute, value| where("lower(#{attribute}) = ?", value.downcase).first }
 end

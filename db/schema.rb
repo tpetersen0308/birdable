c5e874ec525a98b05917e69ad18277706990267a
tpetersen0308 <travis.e.petersen@gmail.com>
2018-10-17 15:13:21 -0700

    add users to db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180829235629) do
+ActiveRecord::Schema.define(version: 20181017221139) do
 
   create_table "birds", force: :cascade do |t|
     t.string "common_name"
@@ -38,6 +38,11 @@ ActiveRecord::Schema.define(version: 20180829235629) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
   end
 
 end

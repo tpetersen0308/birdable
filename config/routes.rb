d334bd88e508fa3a957c821e0cb6896c7724f623
tpetersen0308 <travis.e.petersen@gmail.com>
2018-10-17 15:46:38 -0700

    add route to create session and scope users under api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  post "/login" => "sessions#create"
+  
   scope '/api' do
     scope '/v1' do
       get "/birds" => "birds#index"
       get "/birds/:filter" => "birds#index"
       put "/birds/:id" => "birds#update"
+      resources :users
     end
   end
 end

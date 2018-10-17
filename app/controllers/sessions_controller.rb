f78a68969eed9ea9fa984b9dd403b1438387bf65
tpetersen0308 <travis.e.petersen@gmail.com>
2018-10-17 15:40:31 -0700

    add create action to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,12 @@
 class SessionsController < ApplicationController
+
+  def create
+    @user = User.ci_find("email", params[:email]).first
+
+    if @user && @user.authenticate(params[:password])
+      render json: @user, status: 201
+    else
+      render :json => { :errors => @user.errors}, status: 422
+    end
+  end
 end

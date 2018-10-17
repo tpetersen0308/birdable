73fdb314227e646a1b5820f74f525ac3ae909b66
tpetersen0308 <travis.e.petersen@gmail.com>
2018-10-17 16:45:55 -0700

    add REMOVE_USER case to remove user from the redux store

--- client/src/reducers/usersReducer.js
+++ client/src/reducers/usersReducer.js
@@ -4,6 +4,8 @@ function usersReducer(state = null, action) {
   switch (action.type) {
     case "ADD_USER":
       return action.payload;
+    case "REMOVE_USER":
+      return action.payload;
     default:
       return state;
   }

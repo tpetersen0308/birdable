6c98ba05fd7878fb6eced03ea0cd50d07dcad062
tpetersen0308 <travis.e.petersen@gmail.com>
2018-10-19 17:20:02 -0700

    create handleSubmit method to dispatch postUser action on form submit

--- client/src/containers/Register.js
+++ client/src/containers/Register.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Panel, Form, FormGroup, ControlLabel, FormControl, Button, Col } from 'react-bootstrap';
+import { postUser } from '../actions/userActions.js';
 
 class Register extends Component {
 
+  handleSubmit = event => {
+    postUser('/api/v1/users', {
+      email: event.currentTarget[0].value,
+      password: event.currentTarget[1].value,
+      password_confirmation: event.currentTarget[2].value,
+    })
+  }
 
   render() {
+    const postUser = (route, data) => this.props.dispatch(postUser(route, data))
+
     return (
       <div>
         <Panel>
@@ -12,7 +23,7 @@ class Register extends Component {
             <Panel.Title componentClass="h3">Register a new user account below:</Panel.Title>
           </Panel.Heading>
           <Panel.Body>
-            <Form>
+            <Form onSubmit={this.handleSubmit}>
               <FormGroup controlId="formHorizontalEmail">
                 <FormControl type="email" placeholder="Email" />
               </FormGroup>
@@ -36,4 +47,4 @@ class Register extends Component {
   }
 }
 
-export default Register;
+export default connect()(Register);

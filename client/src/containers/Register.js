ce12dfdedd7943fa141d30aaad3bece36a026c4d
tpetersen0308 <travis.e.petersen@gmail.com>
2018-10-19 18:11:31 -0700

    debug dispatch of postUser in handleSubmit

--- client/src/containers/Register.js
+++ client/src/containers/Register.js
@@ -6,15 +6,17 @@ import { postUser } from '../actions/userActions.js';
 class Register extends Component {
 
   handleSubmit = event => {
-    postUser('/api/v1/users', {
-      email: event.currentTarget[0].value,
-      password: event.currentTarget[1].value,
-      password_confirmation: event.currentTarget[2].value,
-    })
+    event.preventDefault();
+    this.props.dispatch(postUser('/api/v1/users', {
+      user: {
+        email: event.currentTarget[0].value,
+        password: event.currentTarget[1].value,
+        password_confirmation: event.currentTarget[2].value,
+      },
+    }));
   }
 
   render() {
-    const postUser = (route, data) => this.props.dispatch(postUser(route, data))
 
     return (
       <div>
@@ -32,7 +34,7 @@ class Register extends Component {
                 <FormControl type="password" placeholder="Password" />
               </FormGroup>
 
-              <FormGroup controlId="formHorizontalPassword">
+              <FormGroup controlId="formHorizontalPasswordConfirm">
                 <FormControl type="password" placeholder="Confirm Password" />
               </FormGroup>
 

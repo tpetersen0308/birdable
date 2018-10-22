14b2e4ae65e485c93c72ffe7bbb6e8918f308c26
tpetersen0308 <travis.e.petersen@gmail.com>
2018-10-22 15:50:12 -0700

    add form components to Login

--- client/src/containers/Login.js
+++ client/src/containers/Login.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Panel, Form, FormGroup, ControlLabel, FormControl, Button, Col } from 'react-bootstrap';
 
 class Login extends Component {
   render() {
     return (
       <div>
-        Don't have an account? {" "}
-        <Link
-          to="/register"
-        >
-          Sign up!
-        </Link>
+        <Panel>
+          <Panel.Heading>
+            <Panel.Title componentClass="h3">Enter your e-mail and password below to log in:</Panel.Title>
+          </Panel.Heading>
+          <Panel.Body>
+            <Form onSubmit={this.handleSubmit}>
+              <FormGroup controlId="formHorizontalEmail">
+                <FormControl type="email" placeholder="Email" onChange={this.handleEmailChange} />
+              </FormGroup>
+
+              <FormGroup controlId="formHorizontalPassword">
+                <FormControl type="password" placeholder="Password" onChange={this.handlePasswordChange} />
+              </FormGroup>
+
+              <FormGroup>
+                <Button type="submit">Sign in</Button>
+              </FormGroup>
+            </Form>
+            Don't have an account? {" "}
+            <Link
+              to="/register"
+            >
+              Sign up!
+            </Link>
+          </Panel.Body>
+        </Panel>
       </div>
     )
   }

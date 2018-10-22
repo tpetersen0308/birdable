b235253041d5c159a54b89591cd05916877051ed
tpetersen0308 <travis.e.petersen@gmail.com>
2018-10-22 15:45:12 -0700

    add id to session on user authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
     @user = User.ci_find("email", params[:email]).first
 
     if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       render json: @user, status: 201
     else
       render :json => { :errors => @user.errors}, status: 422

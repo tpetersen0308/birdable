4d8aeb5b448bd10c97ba76c8568987eb33ee372f
tpetersen0308 <travis.e.petersen@gmail.com>
2018-10-22 16:13:07 -0700

    debug sessions#create so it successfully finds user based on params

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@ class SessionsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    @user = User.ci_find("email", params[:email]).first
-
-    if @user && @user.authenticate(params[:password])
+    @user = User.ci_find("email", params[:user][:email])
+  
+    if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       render json: @user, status: 201
     else

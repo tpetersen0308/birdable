f768be7f917490cf2e0439080501cdb6e6ba8597
tpetersen0308 <travis.e.petersen@gmail.com>
2018-10-22 15:45:23 -0700

    add id to session on user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       render json: @user, status: 201
     else
       render :json => { :errors => @user.errors}, status: 422

43d73b1813eaaeccdeb2ea3f7f3431ddfef701e4
tpetersen0308 <travis.e.petersen@gmail.com>
2018-10-22 16:34:50 -0700

    add state to form container component

--- client/src/containers/FormContainer.js
+++ client/src/containers/FormContainer.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 class FormContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      password_confirmation: '',
+    }
+  }
+
   render() {
     return (
       <div>

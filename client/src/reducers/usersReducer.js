ed15748cf1ed534293443f9bb218cae7abcf8369
tpetersen0308 <travis.e.petersen@gmail.com>
2018-10-23 15:47:25 -0700

    set initial user state properties to null

--- client/src/reducers/usersReducer.js
+++ client/src/reducers/usersReducer.js
@@ -1,6 +1,6 @@
 // usersReducer() adds the current user to the redux store
 
-function usersReducer(state = null, action) {
+function usersReducer(state = { id: null, email: null, errors: null }, action) {
   switch (action.type) {
     case "ADD_USER":
       return action.payload;

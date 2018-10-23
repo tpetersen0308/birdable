6f32d563898dcc40fd5c230cf1c13c9cd24d735c
tpetersen0308 <travis.e.petersen@gmail.com>
2018-10-22 17:40:38 -0700

    refactor as a stateless presentational component

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,79 +1,34 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import { Panel, Form, FormGroup, ControlLabel, FormControl, Button, Col } from 'react-bootstrap';
-import { postUser } from '../actions/userActions.js';
 
-class Register extends Component {
-  constructor() {
-    super();
-    this.state = {
-      email: '',
-      password: '',
-      password_confirmation: '',
-    }
-  }
+export const Register = (props) => {
 
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.dispatch(postUser('/api/v1/users', {
-      user: this.state,
-    }));
-  }
+  return (
+    <div>
+      <Panel>
+        <Panel.Heading>
+          <Panel.Title componentClass="h3">Register a new user account below:</Panel.Title>
+        </Panel.Heading>
+        <Panel.Body>
+          <Form onSubmit={props.handleSubmit}>
+            <FormGroup controlId="formHorizontalEmail">
+              <FormControl type="email" placeholder="Email" onChange={props.handleEmailChange} />
+            </FormGroup>
 
-  handleEmailChange = event => {
-    event.preventDefault();
-    this.setState({
-      ...this.state,
-      email: event.target.value,
-    });
-  }
+            <FormGroup controlId="formHorizontalPassword">
+              <FormControl type="password" placeholder="Password" onChange={props.handlePasswordChange} />
+            </FormGroup>
 
-  handlePasswordChange = event => {
-    event.preventDefault();
-    this.setState({
-      ...this.state,
-      password: event.target.value,
-    });
-  }
+            <FormGroup controlId="formHorizontalPasswordConfirm">
+              <FormControl type="password" placeholder="Confirm Password" onChange={props.handlePasswordConfirmChange} />
+            </FormGroup>
 
-  handlePasswordConfirmChange = event => {
-    event.preventDefault();
-    this.setState({
-      ...this.state,
-      password_confirmation: event.target.value,
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <Panel>
-          <Panel.Heading>
-            <Panel.Title componentClass="h3">Register a new user account below:</Panel.Title>
-          </Panel.Heading>
-          <Panel.Body>
-            <Form onSubmit={this.handleSubmit}>
-              <FormGroup controlId="formHorizontalEmail">
-                <FormControl type="email" placeholder="Email" onChange={this.handleEmailChange} />
-              </FormGroup>
-
-              <FormGroup controlId="formHorizontalPassword">
-                <FormControl type="password" placeholder="Password" onChange={this.handlePasswordChange} />
-              </FormGroup>
-
-              <FormGroup controlId="formHorizontalPasswordConfirm">
-                <FormControl type="password" placeholder="Confirm Password" onChange={this.handlePasswordConfirmChange} />
-              </FormGroup>
-
-              <FormGroup>
-                <Button type="submit">Sign up</Button>
-              </FormGroup>
-            </Form>
-          </Panel.Body>
-        </Panel>
-      </div>
-    )
-  }
+            <FormGroup>
+              <Button type="submit">Sign up</Button>
+            </FormGroup>
+          </Form>
+        </Panel.Body>
+      </Panel>
+    </div>
+  )
 }
-
-export default connect()(Register);

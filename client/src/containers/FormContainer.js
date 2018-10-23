7b1d0269905efcb3147229a8525fbfd9ff310a1a
tpetersen0308 <travis.e.petersen@gmail.com>
2018-10-22 17:40:21 -0700

    move event handlers from registration component to form container and pass them back into registration component as props

--- client/src/containers/FormContainer.js
+++ client/src/containers/FormContainer.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import Register from '../components/Register.js';
+import { connect } from 'react-redux';
+import { Register } from '../components/Register.js';
 import Login from '../components/Login.js';
+import { postUser } from '../actions/userActions.js';
 
 class FormContainer extends Component {
   constructor() {
@@ -10,13 +12,53 @@ class FormContainer extends Component {
       password: '',
       password_confirmation: '',
     }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handlePasswordConfirmChange = this.handlePasswordConfirmChange.bind(this);
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.dispatch(postUser('/api/v1/users', {
+      user: this.state,
+    }));
+  }
+
+  handleEmailChange = event => {
+    event.preventDefault();
+    this.setState({
+      ...this.state,
+      email: event.target.value,
+    });
+  }
+
+  handlePasswordChange = event => {
+    event.preventDefault();
+    this.setState({
+      ...this.state,
+      password: event.target.value,
+    });
+  }
+
+  handlePasswordConfirmChange = event => {
+    event.preventDefault();
+    this.setState({
+      ...this.state,
+      password_confirmation: event.target.value,
+    });
   }
 
   render() {
     return (
       <div>
         {this.props.history.location.pathname === "/register" ?
-          <Register /> :
+          <Register
+            handleSubmit={this.handleSubmit}
+            handleEmailChange={this.handleEmailChange}
+            handlePasswordChange={this.handlePasswordChange}
+            handlePasswordConfirmChange={this.handlePasswordConfirmChange}
+          /> :
           <Login />
         }
       </div>
@@ -24,4 +66,4 @@ class FormContainer extends Component {
   }
 }
 
-export default FormContainer
+export default connect()(FormContainer);

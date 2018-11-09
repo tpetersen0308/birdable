6a236e985a7cf51347027a179c3bbb3ee56ae691
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-08 18:02:31 -0800

    import secrets object

--- client/src/index.js
+++ client/src/index.js
@@ -14,6 +14,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './reducers/index.js';
+import { secrets } from './conf/secrets.js';
 
 // create Redux store
 const store = createStore(

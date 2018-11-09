6e2eff6d947628c26a864818b02327cb2ff2e5ce
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-08 18:01:42 -0800

    import GoogleLogin component

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GoogleLogin from 'react-google-login';
 import './index.css';
 import App from './containers/App';
 import { Provider } from 'react-redux';

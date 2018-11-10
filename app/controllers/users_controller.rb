0cc87fa803e4e7cf793b83b60089696b70bfc562
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-09 17:41:57 -0800

    add http request to verify id token from google oauth response to users#create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
+require 'httparty'
+
 class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    binding.pry
+    id_token = params[:tokenObj][:id_token]
+    response = HTTParty.get("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=#{id_token}")
+    # check if aud key contains google client id 
+    # if so, create/find user and create session
   end
 
 end

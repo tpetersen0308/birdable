1b3e7b39cd9f3e5ac7e160f5cb181bdf94377ceb
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-09 18:16:26 -0800

    add logic to check for successful id_token verification and create or find and render a user and create a session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,15 @@ class UsersController < ApplicationController
     id_token = params[:tokenObj][:id_token]
     response = HTTParty.get("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=#{id_token}")
     # check if aud key contains google client id 
-    # if so, create/find user and create session
+    if response["aud"] == ENV['GOOGLE_CLIENT_ID']
+      # if so, create/find user and create session
+      @user = User.find_or_create_by(:email => response["email"])
+      @user.update(:first_name => response["given_name"], :last_name => response["family_name"], :image_url => response["picture"])
+      session[:id] = @user.id
+      render json: @user, status: 201
+    else
+      # add error handling here
+    end
   end
 
 end

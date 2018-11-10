ebab5f096d0d7ccfc2e393fc0ebaccb1e2bf702b
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-09 16:19:57 -0800

    add fetch request to postUser action

--- client/src/actions/sessionActions.js
+++ client/src/actions/sessionActions.js
@@ -1,3 +1,14 @@
 export function postUser(data) {
-
+  return fetch("/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "Authorization": data.tokenObj.id_token,
+    },
+    body: JSON.stringify(data),
+  })
+    .then(response => response.JSON())
+    .then(user => {
+      debugger;
+    })
 }

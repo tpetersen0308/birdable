bdd482060f6c29f24ef510535487aa18fcd2e76e
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-12 17:02:40 -0800

    refactor postUser action as a thunk and reformat addCurrentUser and clearCurrentUser actions

--- client/src/actions/sessionActions.js
+++ client/src/actions/sessionActions.js
@@ -1,27 +1,30 @@
 import fetch from 'isomorphic-fetch';
 
 export function postUser(data) {
-  return fetch("/users", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-    },
-    body: JSON.stringify(data),
-  })
-    .then(response => response.json())
-    .then(user => {
-      addCurrentUser(user);
+  return (dispatch) => {
+    return fetch("/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify(data),
     })
+      .then(response => response.json())
+      .then(user => {
+        dispatch(addCurrentUser(user));
+      });
+  }
 }
 
-export function addCurrentUser(data) {
+function addCurrentUser(data) {
   return {
     type: "ADD_CURRENT_USER", payload: data
   }
 }
 
-export function clearCurrentUser() {
+function clearCurrentUser() {
   return {
-    type: "CLEAR_CURRENT_USER", payload: { user: null, logged_in: false }
+    type: "CLEAR_CURRENT_USER", payload: { currentUser: null, loggedIn: false }
   }
 }
+

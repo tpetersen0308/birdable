c3fbea6af6513fc65173d5a53e9bd22a93986365
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-14 15:48:47 -0800

    add image_url_small column to users and assign it from google oauth response in sessions#create

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181109233318) do
+ActiveRecord::Schema.define(version: 20181114234558) do
 
   create_table "birds", force: :cascade do |t|
     t.string "common_name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20181109233318) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url_small"
   end
 
 end

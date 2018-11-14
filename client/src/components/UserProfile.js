ddda3ecf6f9a7e6aba62e93c0fca19ae76465b17
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-14 15:54:26 -0800

    add Panel component with welcome message and user profile image

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Panel, Image } from 'react-bootstrap';
 
-const UserProfile = (props) => {
+export const UserProfile = (props) => {
   return (
     <div>
-
+      <Panel bsStyle="primary" className="user-profile">
+        <Panel.Heading>
+          <Panel.Title componentClass="h3">Welcome to birdable, {props.user.first_name}!</Panel.Title>
+        </Panel.Heading>
+        <Panel.Body>
+          <Image src={props.user.image_url} circle />
+        </Panel.Body>
+      </Panel>
     </div>
   )
 }
-
-export default UserProfile;

47eda12bbabfb80b6f643f99a0673bddc97a300f
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-14 17:29:37 -0800

    initialize users stats to zero if they are nil

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,8 @@ class SessionsController < ApplicationController
       # if so, create/find user and create session
       @user = User.find_or_create_by(:email => response["email"])
       @user.update(:first_name => response["given_name"], :last_name => response["family_name"], :image_url => response["picture"].gsub("s96-c/photo.jpg", "s400-c/photo.jpg"), :image_url_small => response["picture"])
+      @user.update(:correct_answers => 0) unless @user.correct_answers
+      @user.update(:incorrect_answers => 0) unless @user.incorrect_answers
       session[:id] = @user.id
     else
       @user = User.new

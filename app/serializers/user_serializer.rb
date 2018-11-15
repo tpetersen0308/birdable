f58b298e76a4204864dd1903688b9d2bff1f3b84
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-14 17:29:56 -0800

    add user stats to attributes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :first_name, :last_name, :image_url
+  attributes :id, :email, :first_name, :last_name, :image_url, :correct_answers, :incorrect_answers
 end

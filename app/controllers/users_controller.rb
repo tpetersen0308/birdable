c77041c52a257a251b204a5c811082334cdd73f2
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-14 16:37:32 -0800

    add update action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def update
+    @user = User.find_by(:id => params[:id])
+
+    if params[:correct]
+      @user.correct_answers += 1
+    else
+      @user.incorrect_answers += 1
+    end
+
+    @user.save
+
+    render json: @user
+  end
 end

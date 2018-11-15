0f74e02bb8b2850105655a809a5a104d2b41d5ea
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-15 12:24:05 -0800

    add case to handle updating a birds stats

--- client/src/reducers/birdsReducer.js
+++ client/src/reducers/birdsReducer.js
@@ -7,6 +7,12 @@ function birdsReducer(state = [], action) {
   switch (action.type) {
     case "FETCH_BIRDS":
       return action.payload
+    case "UPDATE_BIRD_STATS":
+      let bird = state.find(b => b.id === action.payload.bird_id);
+      let index = state.indexOf(bird);
+      bird.stats.push(action.payload);
+      state.splice(index, 1, bird);
+      return state;
     default:
       return state
   }

b9b0a73e5ea5ecd78af2a4ba5a18d9ff7fd3c2cf
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-14 16:17:30 -0800

    add correct and incorrect answer columns to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181114234558) do
+ActiveRecord::Schema.define(version: 20181115001522) do
 
   create_table "birds", force: :cascade do |t|
     t.string "common_name"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20181114234558) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_url_small"
+    t.integer "correct_answers"
+    t.integer "incorrect_answers"
   end
 
 end

20fcb05ec4bd6aece7c7139f5d21ee1017e8190d
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-15 12:22:39 -0800

    add actions to update user and bird stats in the store and call them in postStat action. Refactor postStat as a thunk

--- client/src/actions/statActions.js
+++ client/src/actions/statActions.js
@@ -2,15 +2,29 @@ import fetch from 'isomorphic-fetch';
 
 export function postStat(user_id, bird_id, data) {
   let url = `/stats/birds/${bird_id}`.concat(user_id ? `/users/${user_id}` : "");
-  return fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-    },
-    body: JSON.stringify(data),
-  })
-    .then(response => response.json())
-    .then(stat => {
-      console.log(stat);
+  return (dispatch) => {
+    return fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify(data),
     })
+      .then(response => response.json())
+      .then(stat => {
+        console.log(stat);
+        dispatch(updateBirdStats(stat));
+        if (user_id) {
+          dispatch(updateUserStats(stat));
+        }
+      })
+  }
+}
+
+function updateUserStats(stat) {
+  return { type: "UPDATE_USER_STATS", payload: stat }
+}
+
+function updateBirdStats(stat) {
+  return { type: "UPDATE_BIRD_STATS", payload: stat }
 }

a800179ea8a6efb97d2c443db495e7a9f9469bd6
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-14 18:24:49 -0800

    add create action to stats controller

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,2 +1,17 @@
 class StatsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    @stat = Stat.new
+
+    if params[:user_id]
+      @stat = Stat.create(:correct => params[:correct == "true"])
+      @stat.user = User.find_by(:id => params[:user_id])
+    end
+
+    @stat.bird = Bird.find_by(:id => params[:bird_id])
+    @stat.save
+
+    render json: @stat
+  end
 end

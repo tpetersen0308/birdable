aa9459a8e1a440ea7531d75f2fa685a02eebd46b
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-15 12:55:00 -0800

    add stats field to user profile with calculated identification rate

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Panel, Image } from 'react-bootstrap';
+import { Panel, Image, ListGroupItem } from 'react-bootstrap';
 
 export const UserProfile = (props) => {
+  const identificationRate = (stats) => {
+    return Math.floor((stats.filter(stat => stat.correct === true).length / stats.length) * 100);
+  }
   return (
     <div>
       <Panel bsStyle="primary" className="user-profile">
@@ -10,6 +13,8 @@ export const UserProfile = (props) => {
         </Panel.Heading>
         <Panel.Body>
           <Image src={props.user.image_url} circle />
+          <h4>Your Stats:</h4>
+          <ListGroupItem><strong>Identification Rate:</strong> {identificationRate(props.user.stats)}%</ListGroupItem>
         </Panel.Body>
       </Panel>
     </div>

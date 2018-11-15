d85e60d2a8ad341c47c32fa0e74192d3dc2645a7
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-15 12:21:38 -0800

    remove correct_answers and incorrect_answers from attributes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :first_name, :last_name, :image_url, :correct_answers, :incorrect_answers
+  attributes :id, :email, :first_name, :last_name, :image_url
   has_many :birds
   has_many :stats
 end

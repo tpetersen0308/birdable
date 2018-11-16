8ae2d416323ad0c05ffe8daa7c56c9366c846f26
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-16 14:31:02 -0800

    add fontawesome stylesheet

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -25,6 +25,7 @@ class App extends Component {
     return (
       <div className="App">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous" />
         <Router>
           <div>
             <header className="App-header">

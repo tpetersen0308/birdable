adddd2f13e78afaaefadb18a300036c4c75e081e
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-16 15:51:10 -0800

    refactor action to add bird to favorites as thunk, add action to update current user and dispatch it in addBirdToFavorites

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,7 +1,7 @@
 import fetch from 'isomorphic-fetch';
 
 export function addBirdToFavorites(user_id, data) {
-  {
+  return (dispatch) => {
     return fetch(`/users/${user_id}`, {
       method: "PUT",
       headers: {
@@ -11,7 +11,12 @@ export function addBirdToFavorites(user_id, data) {
     })
       .then(response => response.json())
       .then(user => {
+        dispatch(updateCurrentUser(user));
         console.log(`${user.first_name}'s favorites successfully updated.`)
       })
   }
+}
+
+function updateCurrentUser(user) {
+  return { type: "UPDATE_CURRENT_USER", payload: user }
 }

7636ede4ea08d99c8028883f51b04273ebea06cf
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-16 14:45:43 -0800

    stub out like button component

---
+++ client/src/components/LikeButton.js
@@ -0,0 +1,11 @@
+import React, { component } from 'react';
+
+class LikeButton extends Component {
+  render() {
+    return (
+      <i class="fas fa-heart"></i>
+    )
+  }
+}
+
+export default LikeButton;

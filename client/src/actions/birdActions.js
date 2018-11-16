71264aa25548787933260e93f0401aeadd7ae71a
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-16 15:37:18 -0800

    remove update stats action that is no longer necessary with new stat actions function

--- client/src/actions/birdActions.js
+++ client/src/actions/birdActions.js
@@ -16,23 +16,3 @@ export function fetchBirds() {
       });
   };
 }
-
-/*
-    updateBirdStats() fires a PUT request to the Rails API to update answer 
-    stats for a given bird.
-*/
-export function updateBirdStats(bird_id, data) {
-  return fetch(`/api/v1/birds/${bird_id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-    },
-    body: JSON.stringify(data),
-  })
-    .then(response => response.json())
-    .then(bird => {
-      console.log(`Bird id#${bird.id}, ${toTitleCase(bird.common_name)}'s stats successfully updated.\n  
-        Stat: ${data.correct ? "Correct Answers" : "Incorrect Answers"}\n  
-        New Value: ${data.correct ? bird.correct_answers : bird.incorrect_answers}`)
-    })
-}
--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,21 +1,5 @@
 import fetch from 'isomorphic-fetch';
 
-export function updateUserStats(user_id, data) {
-  return fetch(`/users/${user_id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-    },
-    body: JSON.stringify(data),
-  })
-    .then(response => response.json())
-    .then(user => {
-      console.log(`${user.first_name}'s stats successfully updated.\n  
-        Stat: ${data.correct ? "Correct Answers" : "Incorrect Answers"}\n  
-        New Value: ${data.correct ? user.correct_answers : user.incorrect_answers}`)
-    })
-}
-
 export function addBirdToFavorites(user_id, data) {
   {
     return fetch(`/users/${user_id}`, {

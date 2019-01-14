0df419055a277b6dcb309818a380073d1c1c18af
tpetersen0308 <travis.e.petersen@gmail.com>
2019-01-14 15:08:33 -0800

    refactor instantiation of Bird and Regions for Bird tests and add test for common name data type

--- spec/bird_spec.rb
+++ spec/bird_spec.rb
@@ -1,9 +1,29 @@
 require "rails_helper"
 
-describe Bird do 
-  it "has a :common_name attribute" do
-    bird = Bird.new(:common_name => "Sedge Wren")
-    expect(bird.attributes.keys).to include("common_name")
-  end
+describe Bird do
+  bird = Bird.new(
+    common_name: "black-footed albatross", 
+    scientific_name: "phoebastria nigripes", 
+    image: "https://cdn.audubon.org/cdn/farfuture/onl3tDMdXUmZ...", 
+    song: "https://cdn.audubon.org/cdn/farfuture/FddSaHlvdtcl...",
+    family: "albatrosses", 
+    url: "https://www.audubon.org/field-guide/bird/black-foo...", 
+    correct_answers: 0, 
+    incorrect_answers: 0)
+  
+  bird.regions << [Region.new(name: "california"), Region.new(name: "northwest"), Region.new(name: "alaska and the north"), Region.new(name: "western canada")]
 
+  describe "attributes" do
+    
+    describe "common name" do
+      it "exists" do
+        expect(bird.attributes.keys).to include("common_name")
+      end
+
+      it "is a string" do
+        expect(bird.common_name.class).to eq String
+      end
+    end
+
+  end
 end

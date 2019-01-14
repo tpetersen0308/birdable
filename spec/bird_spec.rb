d5a9c1bee46b83dd9fb70c55d74561a8ab078f4b
tpetersen0308 <travis.e.petersen@gmail.com>
2019-01-14 14:57:33 -0800

    add test for bird common name presence

--- spec/bird_spec.rb
+++ spec/bird_spec.rb
@@ -1,5 +1,9 @@
 require "rails_helper"
 
 describe Bird do 
+  it "has a :common_name attribute" do
+    bird = Bird.new(:common_name => "Sedge Wren")
+    expect(bird.attributes.keys).to include("common_name")
+  end
 
 end

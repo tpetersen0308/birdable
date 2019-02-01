d0d6bac917f8395240196f8742342ddc8d063604
tpetersen0308 <travis.e.petersen@gmail.com>
2019-01-31 17:51:59 -0800

    refactor fetchBirds to pass filter params so backend can be responsible for filtering logic and pass in action type parameter to determine which action to dispatch on success. Add action to fetch a random bird from backend API

--- client/src/actions/birdActions.js
+++ client/src/actions/birdActions.js
@@ -1,17 +1,30 @@
 import fetch from 'isomorphic-fetch';
-import { toTitleCase } from '../index.js';
 
 /* 
     fetchBirds() fires GET request to the Rails API for the bird collection
     and dispatches an action to add the collection to state.
 */
-export function fetchBirds() {
+export function fetchBirds(filterParams = { families: [], regions: [], favorites: [] }, actionType) {
+  let url = '/api/v1/birds/' + JSON.stringify(filterParams);
   return (dispatch) => {
     dispatch({ type: 'LOADING_BIRDS' });
-    return fetch('/api/v1/birds')
+    return fetch(url)
       .then(response => response.json())
       .then(birds => {
-        dispatch({ type: 'FETCH_BIRDS', payload: birds })
+        dispatch({ type: actionType, payload: birds })
+        dispatch({ type: 'DONE_LOADING' })
+      });
+  };
+}
+
+export function fetchBird() {
+  let url = '/api/v1/birds/';
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_BIRDS' });
+    return fetch(url)
+      .then(response => response.json())
+      .then(bird => {
+        dispatch({ type: 'FETCH_BIRD', payload: bird })
         dispatch({ type: 'DONE_LOADING' })
       });
   };

662ab1bb7b7c78d6e87e3de42f7879252dba5a60
tpetersen0308 <travis.e.petersen@gmail.com>
2019-01-31 18:04:00 -0800

    refactor reducer according to new store structure accommodating new fetching protocol

--- client/src/reducers/birdsReducer.js
+++ client/src/reducers/birdsReducer.js
@@ -3,16 +3,21 @@
     objects from GET request to Rails API to be added to state.
 */
 
-function birdsReducer(state = [], action) {
+function birdsReducer(state = {
+  birds: [],
+  randomBird: []
+}, action) {
   switch (action.type) {
     case "FETCH_BIRDS":
-      return action.payload
+      return { ...state, birds: action.payload }
     case "UPDATE_BIRD_STATS":
-      let bird = state.find(b => b.id === action.payload.bird_id);
-      let index = state.indexOf(bird);
+      let bird = state.birds.find(b => b.id === action.payload.bird_id);
+      let index = state.birds.indexOf(bird);
       bird.stats.push(action.payload);
-      state.splice(index, 1, bird);
+      state.birds.splice(index, 1, bird);
       return state;
+    case "FETCH_RANDOM_BIRD":
+      return { ...state, randomBird: action.payload }
     default:
       return state
   }

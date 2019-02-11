f3c7b61c9b6eb7930335e9c27fdeeeeda8d09361
tpetersen0308 <travis.e.petersen@gmail.com>
2019-02-11 15:10:42 -0800

    refactor to return birds collection based on url filter params

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,19 +1,28 @@
+require "json"
+
 class BirdsController < ApplicationController
   skip_before_action :verify_authenticity_token
   
   # BirdsController#index returns a collection of birds based on filter param, if present,
   # and returns the entire collection of birds if no filter param is present.
   def index
-    if Region.regions.include?(params[:filter])
-      @birds = Bird.by_region(params[:filter])
-    elsif Bird.taxonomic_families.include?(params[:filter])
-      @birds = Bird.by_family(params[:filter])
-    elsif params[:filter]
-      @birds = Bird.all.select{|bird| bird.url_safe_attribute("common_name") == params[:filter]}
-    else
-      @birds = Bird.all.sort_by{|bird| bird.common_name}
-    end
 
+    if !!params["filter"]
+      filters = JSON.parse(params["filter"])
+
+      @birds = filters["families"].map{|family| Bird.by_family(family)}
+      @birds << filters["regions"].map{|region| Bird.by_region(region)}
+      @birds << Bird.where({id: filters["favorites"]})
+      @birds = @birds.flatten.uniq
+  
+      if @birds.length == 0
+        @birds = Bird.all
+      end
+  
+    else
+      @birds = Bird.find_by(id: 1 + rand(Bird.last.id))
+    end
+    
     render json: @birds
   end
 end

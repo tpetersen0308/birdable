b88ea6d60c451e239b803c0337dcdd9871399536
tpetersen0308 <travis.e.petersen@gmail.com>
2019-02-11 16:27:07 -0800

    extract logic for finding a users top birds to rails controller and model

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Panel, Image, ListGroup, ListGroupItem, Table } from 'react-bootstrap';
 import { toTitleCase } from '../index.js';
+import { connect } from 'react-redux';
+import { fetchTopBirds } from '../actions/userActions';
 import LikeButton from './LikeButton.js';
 
-export const UserProfile = (props) => {
-  const identificationRate = (stats) => {
+class UserProfile extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.fetchTopBirds(this.props.user.currentUser.id);
+  }
+
+  identificationRate = (stats) => {
     return Math.floor((stats.filter(stat => stat.correct === true).length / stats.length) * 100) || 0;
   }
 
-  const getFavoriteBirds = (user) => {
+  getFavoriteBirds = (user) => {
     return <ListGroupItem id="favorite-birds"><Table>{user.birds.map(bird => {
       return <tr>
         <td>
@@ -22,66 +32,61 @@ export const UserProfile = (props) => {
     })}</Table></ListGroupItem>
   }
 
-
-  const getTopBirds = (stats, birds) => {
-    let correctStatsByBirdId = {};
-    let topBirdIds = [];
-    let topBirdId = null;
-    let max = 0;
-
-    for (let stat of stats.filter(stat => stat.correct === true)) {
-      correctStatsByBirdId[stat.bird_id] = correctStatsByBirdId[stat.bird_id] + 1 || 1;
-    }
-
-    let n = Object.keys(correctStatsByBirdId).length >= 5 ? 5 : Object.keys(correctStatsByBirdId).length;
-
-    for (let i = 0; i < n; i++) {
-      for (let id in correctStatsByBirdId) {
-        if (max < correctStatsByBirdId[id]) {
-          max = correctStatsByBirdId[id];
-          topBirdId = id;
-        }
-      }
-      topBirdIds.push(topBirdId);
-      delete correctStatsByBirdId[topBirdId];
-      max = 0;
-    }
-    if (topBirdIds.length > 0) {
-      return <ol className="top-birds-list">{topBirdIds.map(id => toTitleCase(birds.find(bird => bird.id === parseInt(id)).common_name)).map(bird => {
-        return <li>{bird}</li>
+  getTopBirds = (topBirds) => {
+    if (topBirds && topBirds.length > 0) {
+      return <ol className="top-birds-list">{topBirds.map(bird => {
+        return <li>{toTitleCase(bird.common_name)}</li>
       })}</ol>
     } else {
       return "You have not yet identified any birds."
     }
   }
-  return (
-    <div>
-      <Panel bsStyle="primary" className="user-profile">
-        <Panel.Heading>
-          <Panel.Title componentClass="h3">Welcome to birdable, {props.user.first_name}!</Panel.Title>
-        </Panel.Heading>
-        <Panel.Body>
-          <Image src={props.user.image_url} circle />
-          <h4>Your Stats:</h4>
-          <ListGroup>
-            {props.loading ? <ListGroupItem>Loading...</ListGroupItem> :
+
+  render() {
+    return (
+      <div>
+        <Panel bsStyle="primary" className="user-profile">
+          <Panel.Heading>
+            <Panel.Title componentClass="h3">Welcome to birdable, {this.props.user.currentUser.first_name}!</Panel.Title>
+          </Panel.Heading>
+          <Panel.Body>
+            <Image src={this.props.user.currentUser.image_url} circle />
+            <h4>Your Stats:</h4>
+            <ListGroup>
+              {this.props.loading ? <ListGroupItem>Loading...</ListGroupItem> :
+                <div>
+                  <ListGroupItem><strong>Identification Rate:</strong> {this.identificationRate(this.props.user.currentUser.stats)}%</ListGroupItem>
+                  <ListGroupItem><strong>Top Birds:</strong> {this.getTopBirds(this.props.user.topBirds)}</ListGroupItem>
+                </div>
+              }
+            </ListGroup>
+            {this.props.user.currentUser.birds.length > 0 &&
               <div>
-                <ListGroupItem><strong>Identification Rate:</strong> {identificationRate(props.user.stats)}%</ListGroupItem>
-                <ListGroupItem><strong>Top Birds:</strong> {getTopBirds(props.user.stats, props.birds)}</ListGroupItem>
+                <h4>Your Favorite Birds:</h4>
+                <ListGroup >
+                  {this.getFavoriteBirds(this.props.user.currentUser)}
+                </ListGroup>
               </div>
             }
-          </ListGroup>
-          {props.user.birds.length > 0 &&
-            <div>
-              <h4>Your Favorite Birds:</h4>
-              <ListGroup >
-                {getFavoriteBirds(props.user)}
-              </ListGroup>
-            </div>
-          }
-        </Panel.Body>
-      </Panel>
-    </div>
-  )
+          </Panel.Body>
+        </Panel>
+      </div>
+    )
+  }
 }
+
+function mapStateToProps(state) {
+  return {
+    user: state.user,
+    loading: state.loading,
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    fetchTopBirds: user_id => dispatch(fetchTopBirds(user_id)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserProfile);
 
